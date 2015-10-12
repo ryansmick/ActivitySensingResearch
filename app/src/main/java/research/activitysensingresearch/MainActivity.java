01404c0be2f1ce238f13fd0537fb3343dd739f49
Courtney Kelly <ckelly19@nd.edu>
2015-10-12 13:30:59 -0400

    Addition of Gyroscope Sensor 	- There is one unresolved issue with "rotationCurrent"

--- app/src/main/java/research/activitysensingresearch/MainActivity.java
+++ app/src/main/java/research/activitysensingresearch/MainActivity.java
@@ -16,35 +16,47 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
+    // Initialize Sensors
+        private SensorManager mSensorManager;
+            private Sensor mAccelerometer;
+            private Sensor mGyroscope;
 
-    private Button mStartButton;
-    private Button mStopButton;
+    // Initialize Buttons for UI
+        private Button mStartButton;
+        private Button mStopButton;
 
-    private static final String TAG = "MainActivity";
+    // Initialize values for Gyroscope calculation
+        private static final String TAG = "MainActivity";
+        private static final float NS2S = 1.0f / 1000000000.0f;
+        private final float[] deltaRotationVector = new float[4]();
+        private float timestamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        // Declare Sensors
+            mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+                mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-        mStartButton = (Button) findViewById(R.id.start_button);
-        mStartButton.setOnClickListener(new View.OnClickListener() {
+        // Start Collecting Sensor Data on push of Start Button
+            mStartButton = (Button) findViewById(R.id.start_button);
+            mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mSensorManager.registerListener(MainActivity.this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+                mSensorManager.registerListener(MainActivity.this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
             }
         });
 
-        mStopButton = (Button) findViewById(R.id.stop_button);
-        mStopButton.setOnClickListener(new View.OnClickListener() {
+        // Stop Collecting Sensor Data on push of Stop Button
+            mStopButton = (Button) findViewById(R.id.stop_button);
+            mStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mSensorManager.unregisterListener(MainActivity.this);
+                    mSensorManager.unregisterListener(MainActivity.this);
             }
         });
     }
@@ -57,16 +69,50 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        // Gravity constants in the x,y,z direction respectively
-        double gravity[] = {0, 9.81, 0};
-        double linear_acceleration[] = new double[3];
+        // Accelerometer
+            // Gravity constants in the x,y,z direction respectively
+            double gravity[] = {0, 9.81, 0};
+            double linear_acceleration[] = new double[3];
 
-        // Remove the gravity contribution with the high-pass filter.
-        linear_acceleration[0] = event.values[0] - gravity[0];
-        linear_acceleration[1] = event.values[1] - gravity[1];
-        linear_acceleration[2] = event.values[2] - gravity[2];
+            // Remove the gravity contribution with the high-pass filter.
+            linear_acceleration[0] = event.values[0] - gravity[0];
+            linear_acceleration[1] = event.values[1] - gravity[1];
+            linear_acceleration[2] = event.values[2] - gravity[2];
 
-        Log.d(TAG, linear_acceleration[0] + "," + linear_acceleration[1] + "," + linear_acceleration[2]);
+            Log.d(TAG, linear_acceleration[0] + "," + linear_acceleration[1] + "," + linear_acceleration[2]);
+
+        // Gyroscope
+            if (timestamp !=0 ) {
+                final float dT = (event.timestamp - timestamp) * NS2S;
+                // Axis of rotation sample (not normalized yet)
+                    float axisX = event.values[0];
+                    float axisY = event.values[1];
+                    float axisZ = event.values[2];
+
+                // Calculate angular speed of the sample
+                    float omegaMagnitude = (float) Math.sqrt(axisX * axisX + axisY * axisY + axisZ * axisZ);
+
+                // Integrate around this axis with the angular speed by the timestep in order to get a delta
+                // rotation from this sample over the timestep. We will convert this axis-angle representation
+                // of the delta rotation into a quaternion before turning it into the rotation matrix.
+
+                    float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+                    float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
+                    float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
+                    deltaRotationVector[0] = sinThetaOverTwo * axisX;
+                    deltaRotationVector[1] = sinThetaOverTwo * axisY;
+                    deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+                    deltaRotationVector[3] = cosThetaOverTwo;
+            }
+
+            timestamp = event.timestamp;
+            float[] deltaRotationMatrix = new float[9];
+            SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+
+            rotationCurrent = rotationCurrent*deltaRotationMatrix;
+
+        }
+
     }
 
     @Override
