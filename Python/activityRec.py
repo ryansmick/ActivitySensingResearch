e7953b76bbee713cbadb2500f2c837d777b56920
Courtney Kelly <ckelly19@nd.edu>
2015-12-03 16:49:13 -0500

    Created Data files, and beginnings of python program

---
+++ Python/activityRec.py
@@ -0,0 +1,5 @@
+#! /user/bin/env python3
+
+# this program will use the SciKit to recongnize which type of motion the app is executing. We will test this on  our three collected data files from our app. We have files for walking, running, and lying down. 
+
+print ("hello")
