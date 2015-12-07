f373134f7282317d3218ec25a1e91202d2184bfa
Courtney Kelly <ckelly19@nd.edu>
2015-12-06 23:25:29 -0500

    Reading data into arrays with python

--- Python/activityRec.py
+++ Python/activityRec.py
@@ -1,4 +1,4 @@
-#! /user/bin/env python3
+#! /user/bin/env python
 
 # this program will use the SciKit to recongnize which type of motion the app is executing. We will test this on  our three collected data files from our app. We have files for walking, running, and lying down. 
 
@@ -11,15 +11,12 @@ testData = []
 
 
 # Load the data from the csv file
-with open('2d_example.csv', 'rb') as csvfile:
+with open('walkingTable.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile)
     i = 0
     for row in reader:
         i = i + 1
-        if i==1:
-            continue
-        X.append(row[1:3])
-        Y.append(row[3])
+        print(row)
 
 
 clf = svm.SVC()
