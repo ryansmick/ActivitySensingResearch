a5a7324ca48a42e8da9e8589bf39734f1f419f66
Courtney Kelly <ckelly19@nd.edu>
2015-12-09 13:46:34 -0500

    testing python program

--- Python/activityRec.py
+++ Python/activityRec.py
@@ -2,12 +2,14 @@
 
 # this program will use the SciKit to recongnize which type of motion the app is executing. We will test this on  our three collected data files from our app. We have files for walking, running, and lying down. 
 
+import numpy
+import scipy
 import csv
 from sklearn import svm
 
 trainingData = []
 classifications = ["Walking", "Upstairs", "Lying Down"]
-testData = []
+testData = numpy.array([])
 walking = []
 upstairs = []
 lyingDown = []
@@ -45,6 +47,7 @@ with open('stairsTable.csv', 'rb') as csvfile:
                 i = i + 1
             except ValueError, e:
                 print "Error: ", e, " in upstairs"
+
 # Load data from lyingDownTable.csv file
 with open('lyingDownTable.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile)
@@ -60,10 +63,11 @@ with open('lyingDownTable.csv', 'rb') as csvfile:
                 i = i + 1
             except ValueError, e:
                 print "Error: ", e, " in lying down"
+    
 
 
 # Load test data
-with open('walkingTestData.csv', 'rb') as csvfile:
+with open('stairsTestData.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile)
     i = 0
     for row in reader:
@@ -71,11 +75,12 @@ with open('walkingTestData.csv', 'rb') as csvfile:
             if i==stop:
                 break
             try:
-                testData.append(float(word))
+                testData = numpy.append(testData,float(word))
                 i = i + 1
             except TypeError, e:
                 print "Error: ", e, " in test data"
 
+testData = numpy.reshape(testData, (1,-1) )
 
 trainingData.append(walking)
 trainingData.append(upstairs)
